88658c24bc18874d6e04f35d005eb53bcfaa77d7
Hesam Pirzadeh <pirzadehhesam00@gmail.com>
2024-06-07 04:23:59 +0330

    create input and create category handler

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,22 +1,62 @@
+import { IoIosSearch } from "react-icons/io";
+import { FaListUl } from "react-icons/fa";
 import Card from "../components/Card";
 import Loader from "../components/Loader";
+
 import { useProduct } from "../context/ProductContext";
 
 import styles from "../css/ProductPages.module.css";
+import { useState } from "react";
 
 function ProductsPage() {
+  const [input, setInput] = useState([]);
   const products = useProduct();
 
+
+  const searchHanlder =()=>{
+    console.log("search");
+  }
+
+  const CategoryHandler =(e)=>{
+    const {tagName} = e.target 
+    const list = e.target.innerText.toLowerCase()
+    
+    if (tagName !== "LI") return 
+  }
+
   return (
-    <div className={styles.container}>
-      {!products.length && <Loader/>}
-      <div className={styles.products}>
-        {products.map((p) => (
-          <Card key={p.id} data={p} />
-        ))}
+    <>
+      <div>
+        <input
+          type="text"
+          placeholder="Search..."
+          value={input}
+          onChange={(e) => setInput(e.target.value.toLowerCase().trim())}
+        />
+        <button onClick={searchHanlder}>
+          <IoIosSearch />
+        </button>
       </div>
-      <div>sidebar</div>
-    </div>
+      <div className={styles.container}>
+        {!products.length && <Loader />}
+        <div className={styles.products}>
+          {products.map((p) => (
+            <Card key={p.id} data={p} />
+          ))}
+        </div>
+        <div>
+          <div><FaListUl/></div>
+          <p>Categories</p>
+        </div>
+        <ul onClick={CategoryHandler}>
+          <li>All</li>
+          <li>Electronics</li>
+          <li>Jewelery</li>
+          <li>Men's Clothing</li>
+          <li>Women's Clothing</li>
+        </ul>
+      </div>
+    </>
   );
 }
 
